fd8558ee105dfdf391e7773c4812c1e558a6fbec
SimonRichardson <stickupkid@gmail.com>
2014-12-04 16:23:33 +0000

    Got merge working

--- generic/tree.go
+++ generic/tree.go
@@ -9,19 +9,19 @@ type Tree interface {
 }
 
 type TreeNode struct {
-	Value Any
+	value Any
 	nodes List
 }
 
 func NewTreeNode(x Any, y List) TreeNode {
 	return TreeNode{
-		Value: x,
+		value: x,
 		nodes: y,
 	}
 }
 
 func (t TreeNode) Chain(f func(Any) Tree) Tree {
-	x := f(t.Value)
+	x := f(t.value)
 	if _, ok := x.(TreeNil); ok {
 		return x
 	}
@@ -38,79 +38,41 @@ func (t TreeNode) Map(f func(Any) Any) Tree {
 }
 
 func (t TreeNode) FoldLeft(x Any, f func(Any, Any) Any) Any {
-	return t.nodes.FoldLeft(f(x, t.Value), func(x, y Any) Any {
+	return t.nodes.FoldLeft(f(x, t.value), func(x, y Any) Any {
 		return y.(Tree).FoldLeft(x, f)
 	})
 }
 
-// a
-// | - b, x, y
-//     | - c
-//         | - d, 1, 2
-
-// a
-// | - b, x, y
-//     | - z
-//         | - d, 1, 2
-
-// a, b, x, y, c, d, 1, 2
-// a, b, x, y, z, d, 1, 2
-
-// a
-// | - b, x, y
-//     | - c             z
-//         | - d, 1, 2   | - d, 1, 2
 func (t TreeNode) Merge(m Tree) Tree {
-	var rec func(a, b, c Tree) List
-
-	xxx := func(nodes List) List {
-		return AsList(nodes.Reverse().FoldLeft(NewNil(), func(a, b Any) Any {
-			var (
-				x = AsList(a)
-				y = AsTree(b)
-			)
-			return rec(y, NewTreeNil(), NewTreeNil()).Concat(x)
-		}))
-	}
-	yyy := func(a TreeNode) List {
-		x := xxx(a.nodes)
-		return List_.Of(
-			NewTreeNode(a.Value, x),
-		)
-	}
-
-	rec = func(a, b, c Tree) List {
-		_, ok1 := a.(TreeNil)
-		_, ok2 := b.(TreeNil)
-		if ok1 && ok2 {
-			return List_.Of(c)
-		}
-
-		if !ok1 && ok2 {
-			return yyy(a.(TreeNode))
-		} else if ok1 && !ok2 {
-			return yyy(b.(TreeNode))
-		} else {
-
-			x := a.(TreeNode)
-			y := b.(TreeNode)
-
-			xNodes := xxx(x.nodes)
-			yNodes := xxx(y.nodes)
-
-			if x.Value == y.Value {
-				return List_.Of(
-					NewTreeNode(x.Value, xNodes.Concat(yNodes)),
-				)
-			} else {
-				return List_.To(
-					NewTreeNode(x.Value, AsList(xNodes)),
-					NewTreeNode(y.Value, AsList(yNodes)),
-				)
+	var (
+		rec func(a, b List) List
+	)
+	rec = func(a, b List) List {
+		return a.Chain(func(x Any) List {
+			if _, ok := x.(TreeNil); ok {
+				return List_.Of(x)
 			}
-		}
+
+			var (
+				node  = x.(TreeNode)
+				val   = node.value
+				nodes = node.nodes
+				clean = b.Filter(func(x Any) bool {
+					node = x.(TreeNode)
+					return node.value != val
+				})
+				others = b.Filter(func(x Any) bool {
+					node = x.(TreeNode)
+					return node.value == val
+				})
+				children = AsList(others.FoldLeft(NewNil(), func(a, b Any) Any {
+					return a.(List).Concat(b.(TreeNode).nodes)
+				}))
+			)
+			return List_.Of(NewTreeNode(val, rec(nodes, children))).Concat(clean)
+		})
 	}
-	return NewTreeNode(".", rec(t, m, NewTreeNil()))
+	return NewTreeNode("/", rec(List_.Of(t), List_.Of(m)))
 }
 
 func (t TreeNode) Children() Option {
